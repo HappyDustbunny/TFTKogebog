c3d18caae8206df30d93aec95a4fdcefe6865011
HappyDustbunny <madshorn@operamail.com>
2022-09-25 14:16:18 +0200

    Add Kål med Hasselnødder, Helstegte Portobello and Ovndampet Sej

--- components/emigrant.js
+++ components/emigrant.js
@@ -5,48 +5,40 @@ class Emigrant extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
     <button class="insertedRecipe">
-     <h3>Emigrant Br&oslash;d</h3>
+    	<h3>Sage and garlic rolls</h3>
     </button>
 
     <div class="insertedIngredienser">
-      <p class="antal">(3 - 4 personer)		(Start 1&frac12; time f&oslash;r)</p>
-      <b>Ingredienser:</b>
+    	<p class="antal">(3 - 4 people) (Start 1&frac12; hours before)</p>
 
-      <p><label><input type="checkbox">  500 g mel</label></p>
-      <p><label><input type="checkbox">  100 g sm&oslash;r (eller bagemargarine)</label></p>
-      <p><label><input type="checkbox">  2 tsk salt</label></p>
-      <p><label><input type="checkbox">  2 fed hvidl&oslash;g</label></p>
-      <p><label><input type="checkbox">  1 tsk t&oslash;rret salvie</label></p>
-      <p><label><input type="checkbox">  25 g g&aelig;r</label></p>
-      <p><label><input type="checkbox">  3 dL m&aelig;lk</label></p>
+    	<b>Ingredienser:</b>
+    	<p><label><input type="checkbox">Ingredients:</label></p>
+    	<p><label><input type="checkbox">500 g flour</label></p>
+    	<p><label><input type="checkbox">100 g butter (or baking margarine)</label></p>
+    	<p><label><input type="checkbox">2 teaspoons of salt</label></p>
+    	<p><label><input type="checkbox">2 cloves of garlic</label></p>
+    	<p><label><input type="checkbox">1 teaspoon dried sage</label></p>
+    	<p><label><input type="checkbox">25 g of yeast</label></p>
+    	<p><label><input type="checkbox">3 dL milk</label></p>
     </div>
 
-    <div class="insertedHowto"><b>Fremgangsm&aring;de:</b>
+    <div class="insertedHowto"> <b> Method: </b>
+    	<p><label><input type="checkbox">Put 500 g of flour in a Margrethe bowl</label></p>
+    	<p><label><input type="checkbox">Add 100 g butter (or baking margarine) cut into small cubes and crumble it together with the flour</label></p>
+    	<p><label><input type="checkbox">Add 1 teaspoon of salt</label></p>
+    	<p><label><input type="checkbox">Add 1 teaspoon of dried sage to the mortar and crush it roughly. Add the sage to the flour mixture</label></p>
+    	<p><label><input type="checkbox">Put 2 cloves of garlic in the garlic press and press them into the flour mixture. Stir until the garlic and spices are evenly distributed</label></p>
+    	<p><label><input type="checkbox">Measure out 3 dL of milk and add 25 g of yeast. Stir until the yeast is dissolved</label></p>
+    	<p><label><input type="checkbox">Add milk and yeast to the flour mixture and knead well</label></p>
+    	<p><label><input type="checkbox">Divide the dough into 16 pieces and form them into small oblong loaves</label></p>
+    	<p><label><input type="checkbox">Let the loaves rise on the baking sheet for approx. 30 minutes</label></p>
+    	<p><label><input type="checkbox">Turn on the oven at 200° (remember to remove the plates)</label></p>
+    	<p><label><input type="checkbox">When the oven is hot, bake the loaves for 12-16 minutes</label></p>
+    	<p><label><input type="checkbox">Take the bread out and cool on a wire rack</label></p>
+    	<p><label><input type="checkbox">Tip: Emigrant Bread is good for soup or as a side dish for vegetable dishes</label></p>
+    	<p><label><input type="checkbox">Tip: The breads are suitable for freezing, so you can consider baking a double portion and freezing half. When you need to use them, you take them out of the freezer a few hours before they are to be used and let them thaw on a wire rack. It is good to heat them for 5 minutes in the oven or on the toaster before serving</label></p>
 
-      <p><label><input type="checkbox">  Kom 500 g mel i en Margrethesk&aring;l</label></p>
-      <p><label><input type="checkbox">  Tils&aelig;t 100 g sm&oslash;r (eller bagemargarine) sk&aring;ret
-         i sm&aring; tern og smuldr det sammen med melet</label></p>
-      <p><label><input type="checkbox">  Tils&aelig;t 1 tsk salt</label></p>
-      <p><label><input type="checkbox">  Kom 1 tsk t&oslash;rret salvie morteren og knus
-        det groft. Tils&aelig;t salvien til melblandingen</label></p>
-      <p><label><input type="checkbox">  Kom 2 fed hvidl&oslash;g i hvidl&oslash;gspresseren
-        og pres dem ned i melblandingen. R&oslash;r rundt til hvidl&oslash;g og krydderier er j&aelig;vnt fordelt</label></p>
-      <p><label><input type="checkbox">  M&aring;l 3 dL m&aelig;lk op og tils&aelig;t 25 g g&aelig;r. R&oslash;r til g&aelig;ren er opl&oslash;st</label></p>
-      <p><label><input type="checkbox">  Kom m&aelig;lk og g&aelig;r i melblandingen og &aelig;lt godt</label></p>
-      <p><label><input type="checkbox">  Del dejen i 16 stykker og form dem til sm&aring; aflange br&oslash;d</label></p>
-      <p><label><input type="checkbox">  Lad br&oslash;ddene h&aelig;ve p&aring; bagepladen i ca. 30 minutter</label></p>
-      <p><label><input type="checkbox">  T&aelig;nd ovnen p&aring; 200&deg; (husk at tage pladerne ud)</label></p>
-      <p><label><input type="checkbox">  N&aring;r ovnen er varm bages br&oslash;dene 12-16 minutter</label></p>
-      <p><label><input type="checkbox">  Br&oslash;denen tages ud og k&oslash;les af p&aring; en rist</label></p>
-      <br>
-      <p>Tip: Emigrant Br&oslash;d er gode til suppe eller som tilbeh&oslash;r til gr&oslash;ntsagsretter</p>
-      <p>Tip: Br&oslash;dene er velegnede til frysning, s&aring; man kan overveje at bage dobbelt portion og
-        fryse halvdelen. N&aring;r man skal bruge dem tager man dem op af fryseren et par timer f&oslash;r de
-        skal bruges og lader dem t&oslash; op p&aring; en rist. Det er godt at varme dem 5 minutter i ovnen
-        eller p&aring; br&oslash;dristeren f&oslash;r de serveres</p>
-
-      <button class="slut">(Slut)</button>
-
+    	<button class="slut">(End)</button>
     </div>
     `;
   }
