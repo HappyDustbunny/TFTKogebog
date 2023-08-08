e5cb44a659762723bf56a6827215a8bb35c22308
HappyDustbunny <madshorn@operamail.com>
2023-08-08 12:46:46 +0200

    Fix details

--- kogebog_serviceworker.js
+++ kogebog_serviceworker.js
@@ -1,5 +1,5 @@
 
-let VERSION = 2.015; // Change version number to facilitate updates
+let VERSION = 2.016; // Change version number to facilitate updates
 let TFTK_CACHE = 'TFTK-cache';
 let CACHED_URLS = [
   'components/modal-init.js', // Copy project path from EVERY entry to avoid mistakes
