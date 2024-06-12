a8848f6d9bb2b3d1a2bcdc7db1bee7f9d02a2927
HappyDustbunny <madshorn@operamail.com>
2024-06-12 14:20:20 +0200

    Change machine save point

--- newRecipeFolder/newRecipeMarkupParser.py
+++ newRecipeFolder/newRecipeMarkupParser.py
@@ -25,25 +25,34 @@ def main():
         read_file = in_file.readlines()
 
         first_line = read_file[0]
-        all_text.append('              <button id="' + first_line.replace(" ", "") + '" class="recipe">\n')
+        current_line = first_line.split()
+        all_text.append('              <button id="' + ''.join(n.capitalize() for n in current_line) + '" class="recipe">\n')
         all_text.append('                  <h3>' + first_line.strip() + '</h3>\n')
         all_text.append('              </button>\n')
         
-        # next_line = read_file.readline()
-        # all_text.append('              <div class="ingredienser">\n')
-        # all_text.append('                  <div class="recipeHeading">\n')
-        # all_text.append('                  <p class="antal">' + next_line + '</p>\n')
-        # all_text.append('                  <button class="share">Del</button>\n')
-        # all_text.append('              </div>\n')
+        next_line = read_file[1]
+        all_text.append('              <div class="ingredienser">\n')
+        all_text.append('                  <div class="recipeHeading">\n')
+        all_text.append('                  <p class="antal">' + re.sub('ø', '&oslash;', next_line, 50) + '</p>\n')
+        all_text.append('                  <button class="share">Del</button>\n')
+        all_text.append('              </div>\n')
+        all_text.append('              <b>Ingredienser:</b>\n')
 
         for line in read_file:
             if line:
+                line = re.sub('æ', '&aelig;', line, 50)
+                line = re.sub('ø', '&oslash;', line, 50)
+                line = re.sub('å', '&aring;', line, 50)
+                line = re.sub('Æ', '&AElig;', line, 50)
+                line = re.sub('Ø', '&Oslash;', line, 50)
+                line = re.sub('Å', '&Aring;', line, 50)
                 chunk.append(line)
-            if line.strip() == 'Ingredienser':
+            if line[0] == '(' and line[-2] == ')':
+                chunk = []  # Done above
+            elif line.strip() == 'Fremgangsm&aring;de':
                 all_text = wrap_ingredients(chunk, all_text)
-                if line.strip():
-                    chunk.append(line)
-            elif line.strip() == 'Slut':  # REMEMBER to remove () here
+                chunk = []
+            elif line.strip() == 'Slut':
                 all_text = wrap_method(chunk, all_text)
 
         for line in all_text:
@@ -54,11 +63,10 @@ def main():
 
 
 def wrap_ingredients(chunk, all_text):
-    all_text.append('<div class="ingredienser">')
-    all_text.append('<p class="antal"> ' + chunk[0].strip() + ' </p>\n')
-    all_text.append('<b> Ingredients: </b>\n')
     for line in chunk:
-        if line[0] == '(' and line[-2] == ')' or line == '\n':
+        if line.strip() == '':
+            all_text.append('\t<br>')
+        elif line.strip() == 'Fremgangsm&aring;de':
             pass
         else:
             all_text.append('\t<p><label><input type="checkbox"> ' + line.strip() + ' </label></p>\n')
@@ -68,14 +76,16 @@ def wrap_ingredients(chunk, all_text):
 
 
 def wrap_method(chunk, all_text):
-    all_text.append('<div class="howto"> <b> Fremgangsm&oring;de: </b>\n')
+    all_text.append('<div class="howto"> <b> Fremgangsm&aring;de: </b>\n')
     for line in chunk:
-        if line.strip() == 'Fremgangsm&oring;de:' or line == '\n':
+        if line.strip() == 'Fremgangsm&aring;de:' or line == 'Slut':
             pass
+        elif line.strip() == '':
+            all_text.append('\t<br>')
         else:
             all_text.append('\t<p><label><input type="checkbox">' + line.strip() + '</label></p>\n')
 
-    all_text.append('\t<button class="slut">(End)</button>')
+    all_text.append('\t<button class="slut">(Slut)</button>')
     all_text.append('</div>\n')
     return all_text
 
