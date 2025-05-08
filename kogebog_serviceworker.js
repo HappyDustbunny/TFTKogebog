6322cb78cf3a0b3a79ded8f173ba550d7789ae67
HappyDustbunny <madshorn@emailcorner.net>
2025-05-08 17:15:54 +0200

    Add recipe for Okonomiyaki

--- kogebog_serviceworker.js
+++ kogebog_serviceworker.js
@@ -1,5 +1,5 @@
 
-let VERSION = 2.025; // Change version number to facilitate updates
+let VERSION = 2.026; // Change version number to facilitate updates
 let TFTK_CACHE = 'TFTK-cache';
 let CACHED_URLS = [
   'components/modal-init.js', // Copy project path from EVERY entry to avoid mistakes
