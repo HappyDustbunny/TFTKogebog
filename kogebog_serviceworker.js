e5585184f0233afd2bdf34911bd23fd32c01c63c
HappyDustbunny <madshorn@emailcorner.net>
2025-05-19 14:11:34 +0200

    Fix small errors, fix æøå and add pasta to Gratinerede Grøntsager

--- kogebog_serviceworker.js
+++ kogebog_serviceworker.js
@@ -1,5 +1,5 @@
 
-let VERSION = 2.027; // Change version number to facilitate updates
+let VERSION = 2.028; // Change version number to facilitate updates
 let TFTK_CACHE = 'TFTK-cache';
 let CACHED_URLS = [
   'components/modal-init.js', // Copy project path from EVERY entry to avoid mistakes
